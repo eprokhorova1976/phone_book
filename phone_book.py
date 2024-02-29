10795a64d6fa69bf2a833cbda8d7c0c4efe7bc57
eprokhorova1976 <154540454+eprokhorova1976@users.noreply.github.com>
2024-02-29 15:07:48 +0200

    Add files via upload
    Кирилл, здравствуйте.
    Справочник доделываю, пока выложила то, что сделала. Я серьёзно болела и сильно отстала от программы обучения, стараюсь наверстать как можно быстрее.
    Справочник делала на базе того, что делали в комнате под запись на 8 семинаре. Постараюсь доделать его сегодня (но поскольку не уверена, что до окончания срока проверки - выложила недоделанный вариант). Но хочу сделать всё сама, а потом смотреть семинары с разбором.

---
+++ phone_book.py
@@ -0,0 +1,99 @@
+import re, string
+
+#Создать контакт
+def add_contact():
+    book = open('phones.txt', 'r+', encoding = 'utf-8')
+    name = input('Введите имя: ')
+    number = input('Введите номер: ')
+    number = re.sub (r'[-]', '', number)
+    comment = input('Введите комментарий: ')
+    id = len(book.readlines())+1
+    book.write(f'\n{id}; {name}; {number}; {comment}')
+    book.close()
+    print (f'Контакт {name}, {number}, {comment} добавлен')
+
+#Открыть справочник, cкопировать контакты
+def take_contacts():
+    with open ('phones.txt', 'r+', encoding='utf-8') as book:
+        return book.readlines()  
+
+#Показать все контакты
+def show_contacts():
+    book = open('phones.txt', 'r', encoding='utf-8')
+    for line in book:
+        print(line)
+
+
+#Найти контакт
+def find_contact():
+    book = take_contacts()
+    what = input ('Введите номер параметра для поиска: 1-имя, 2-номер телефона, 3-комментарий: ')
+    if what == '1':
+        name = input ('Введите имя: ').lower()
+        count = 0
+        for contact in book:
+            if name in contact.lower().split()[1]:
+                print (contact)
+                count +=1
+        if count == 0:
+            make_contact = input ('Такого имени в списке нет. Хотите добавить контакт? 1-да, 2-нет: ')
+            if make_contact == '1':
+                add_contact()
+    elif what == '2':
+        number = input ('Введите номер телефона: ')
+        number = re.sub (r'[-]', '', number)
+        count = 0
+        for contact in book:
+            if number in contact.split()[2]:
+                print (contact)
+                count +=1
+        if count == 0:
+            make_contact = input ('Такого номера телефона в списке нет. Хотите добавить контакт? 1-да, 2-нет: ')
+            if make_contact == '1':
+                add_contact()
+    elif what == '3':
+        comment = input ('Введите комментарий: ').lower()
+        count = 0
+        for contact in book:
+            if comment in contact.lower().split():
+                print (contact)
+                count +=1
+        if count == 0:
+            make_contact = input ('Такого комментария в списке нет. Хотите добавить контакт? 1-да, 2-нет: ')
+            if make_contact == '1':
+                add_contact()
+            else:
+                different_search = input ('Попробовать найти по другим параметрам? 1-да, 2-нет: ')   
+                if different_search == '1':
+                    find_contact() 
+    else:
+        print ('Вы выбрали несуществующий вариант. Попробуйте снова')
+        find_contact()
+                
+#Изменить контакт
+        
+#Удалить контакт
+def delete_contact():
+    book = take_contacts()
+    what = input ('Какой контакт нужно удалить? Введите параметры для поиска: ').lower()
+    count = 0
+    for contact in book:
+        if what in contact.lower().split()[1] or what in contact.split()[2] or what in contact.lower().split():
+            print (contact)
+            count +=1
+    if count != 0:
+        choose_contact = int(input ('Введите номер id удаляемого контакта: '))
+        if choose_contact == contact [0]:
+            book.pop (choose_contact)
+            print (contact)
+    if count == 0:
+        different_search = input ('Такого контакта не нашлось. Попробовать найти по другим параметрам? 1-да, 2-нет: ')   
+        if different_search == '1':
+            delete_contact() 
+    
+
+delete_contact()
+#Сохранить справочник
+#Выход
+
+
